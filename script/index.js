1ce0b850e1d7e852e645664d0197b9cf08bd767a
Andrew DGalga <ex.spake@gmail.com>
2024-01-12 21:56:04 -0600

    test image and add to page.

--- script/index.js
+++ script/index.js
@@ -2,7 +2,7 @@ const hook = document.getElementById("what-do__game-containers");
 const ENGINES = Object.freeze({ UNITY: "./data/engine_icons/unity.svg", GODOT:"../data/engine_icons/godot.svg"});
 
 class WorkBento {
-    constructor({imagePath="", engine="", blurb="", codeLink="", playLink=""}){
+    constructor({parentElement, imagePath="", engine="", blurb="", codeLink="", playLink=""}){
         Object.keys(ENGINES).forEach(element => {
             if(engine.toUpperCase() === element) this.engineImagePath=engine;
         });
@@ -10,12 +10,20 @@ class WorkBento {
         this.blurb = blurb;
         this.codeLink = codeLink;
         this.playLink = playLink;
+        this.parentElement = parentElement;
+    }
+    getBox(){
+        const bod = document.createElement("div");
+        bod.appendChild(this.getImage());
+        this.parentElement.appendChild(bod);
+    }
+    getImage(){
+        const img = document.createElement("img");
+        img.src = this.imagePath;
+        return img;
     }
 }
 
-//const testBento = new WorkBento({engine:"GODOT"});
-/*
-const testImage = document.createElement('img');
-testImage.src = ENGINES.UNITY;
-hook.append(testImage);
-*/
+const testImagePath = "./data/photos/gameTestImage.jpg";
+const testBox = new WorkBento({parentElement:hook, imagePath:testImagePath});
+testBox.getBox();
