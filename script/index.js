d1def4b421260dff624ac30e4a8c66c55e718b38
Andrew DGalga <ex.spake@gmail.com>
2024-01-12 21:43:25 -0600

    silly constructor (silly because looks excessive to me)

--- script/index.js
+++ script/index.js
@@ -2,11 +2,20 @@ const hook = document.getElementById("what-do__game-containers");
 const ENGINES = Object.freeze({ UNITY: "./data/engine_icons/unity.svg", GODOT:"../data/engine_icons/godot.svg"});
 
 class WorkBento {
-    constructor({image, engine, blurb, codeLink, playLink}){
-        
+    constructor({imagePath="", engine="", blurb="", codeLink="", playLink=""}){
+        Object.keys(ENGINES).forEach(element => {
+            if(engine.toUpperCase() === element) this.engineImagePath=engine;
+        });
+        this.imagePath = imagePath;
+        this.blurb = blurb;
+        this.codeLink = codeLink;
+        this.playLink = playLink;
     }
 }
 
+//const testBento = new WorkBento({engine:"GODOT"});
+/*
 const testImage = document.createElement('img');
 testImage.src = ENGINES.UNITY;
 hook.append(testImage);
+*/
