e46a17d71acf53af55695391ce225b1017442963
Andrew DGalga <ex.spake@gmail.com>
2024-02-15 20:08:31 -0600

    testing web section generation. Working so far, though pretty sure I'm not following best practices.

--- script/index.js
+++ script/index.js
@@ -4,6 +4,44 @@ const portfolio_web_sect = document.getElementById("home_grid--web--work");
 const portfolio_game_btn = document.getElementById("home_grid--game--reveal");
 const portfolio_game_sect = document.getElementById("home_grid--game--work");
 
+const data_source = "data/content.json";
+
+/*
+<div class="scroll_item">
+    <img class="scroll_item--photo" src="data/photos/gameTestImage.jpg" alt="test">
+    <h3><a href="#">Lorem Ipsum Dolor</a></h3>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+    <a class="link-button">GitHub</a>
+</div>
+*/
+
+const createWebSection = (jsonObj) => {
+    console.log(jsonObj.web);
+    for(let i = 0; i < Object.keys(jsonObj.web).length;i++) {
+        const itemContainer = document.createElement('div');
+        itemContainer.classList.add("scroll_item");
+        const itemImage = document.createElement('img');
+        itemImage.src = "data/photos/gameTestImage.jpg"; //jsonObj.web.photo-location;
+        itemImage.alt = "test";
+        itemImage.classList.add("scroll_item--photo");
+        portfolio_web_sect.appendChild(itemContainer);
+        itemContainer.appendChild(itemImage);
+        //portfolio_web_sect.
+    }
+    //console.log(Object.keys(jsonObj.web).length);
+}
+
+const data_content = async () => {
+    await fetch(data_source)
+        .then((response)=> response.json())
+        .then((json)=>{
+            createWebSection(json);
+        });
+}
+data_content();
+
 const applyClass = (obj, cssClass) => {
     (obj.classList.contains(cssClass)) ? obj.classList.remove(cssClass) : obj.classList.add(cssClass);
 };
